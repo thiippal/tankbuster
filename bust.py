1056232a075de9a83249a08400c3c3c9d316b775
thiippal <tuomo.hiippala@iki.fi>
2016-09-04 21:25:41 +0300

    initial commit

---
+++ bust.py
@@ -0,0 +1,33 @@
+import os
+import argparse
+import glob
+from buster.engine import bust
+
+# Set up the argument parser
+ap = argparse.ArgumentParser()
+
+# Add argument for input
+ap.add_argument("-i", "--input", required=True, help="The image or directory to be analysed.")
+
+# Parse the arguments
+args = vars(ap.parse_args())
+
+# Assign arguments to variables
+image = args['input']
+
+# Check if the input is a directory
+if os.path.isdir(image):
+
+    # Feed the images to the CNN
+    for images in glob.glob(str(image) + '*.*'):
+        bust(images)
+
+# Check if the input is a file
+if os.path.isfile(image):
+
+    # Feed the image to the CNN
+    bust(image)
+
+
+
+
---
+++ buster/engine/detect.py
@@ -0,0 +1,28 @@
+# Import the necessary packages
+import cv2
+from buster.cnn import CNNArchitecture
+
+def bust(image):
+    # Load and process the image
+    original = cv2.imread(image)
+    resized = cv2.resize(original, (256, 256), interpolation=cv2.INTER_AREA)  # Resize
+    rescaled = resized.astype("float") / 255.0  # Scale into range 0...1
+    reshaped = rescaled.reshape((1,) + rescaled.shape)  # Reshape for input to CNN
+
+    # Load the CNN architecture and pre-trained weights, compile the model
+    model = CNNArchitecture.select('MiniVGGNet', 256, 256, 3, 2)
+    model.load_weights('classifier/weights.hdf5')
+    model.compile(loss='mse', optimizer='sgd', metrics=['accuracy'])
+
+    # Return the prediction
+    prediction = model.predict_proba(reshaped, verbose=0)[0]
+
+    # Check the predicted labels
+    other = prediction[0]
+    tank = prediction[1]
+
+    if tank > 0.10:
+        print "[RESULT] File {} contains a tank with {:.2f}% confidence.".format(image, tank * 100)
+    else:
+        print "[RESULT] File {} does not contain a tank with {:.2f}% confidence.".format(image, other * 100)
+
