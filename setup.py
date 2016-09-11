74132b09a6bef3d3dc03d5e163128f2552fe0148
thiippal <tuomo.hiippala@iki.fi>
2016-09-11 12:40:22 +0300

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 from setuptools.command.install import install
 
 setup(name='tankbuster',
-      version='0.0.3',
+      version='0.1',
       description='An image classifier trained to detect Soviet/Russian T-72 tanks',
       url='https://github.com/thiippal/tankbuster',
       author='Tuomo Hiippala',
@@ -13,7 +13,7 @@ setup(name='tankbuster',
       author_email='tuomo.hiippala@iki.fi',
       license='MIT',
       keywords=['osint', 'imaging', 'classifier'],
-      download_url='https://github.com/thiippal/tankbuster/tarball/0.0.3',
+      download_url='https://github.com/thiippal/tankbuster/tarball/0.1',
       install_requires=["Pillow>=3.3.1",
                         "numpy>=1.11.1",
                         "Keras>=1.0.8",
