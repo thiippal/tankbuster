9c53e81ed030ac3c96661f958a6e9c99c8e32ca9
thiippal <tuomo.hiippala@iki.fi>
2017-04-08 17:27:14 +0300

    prepare for 0.3.1

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@ from setuptools import setup
 from setuptools.command.install import install
 
 setup(name='tankbuster',
-      version='0.3',
+      version='0.3.1',
       description='A neural network trained to detect Soviet/Russian military vehicles in photographs',
       url='https://github.com/thiippal/tankbuster',
       author='Tuomo Hiippala',
       packages=['tankbuster'],
       package_dir={'tankbuster': 'tankbuster'},
-      package_data={'tankbuster': ['*.py', 'engine/*.h5', 'cnn/*.py', 'engine/*.py']},
+      package_data={'tankbuster': ['*.py', 'cnn/*.py', 'engine/*.py']},
       author_email='tuomo.hiippala@iki.fi',
       license='MIT',
       keywords=['osint', 'computer vision', 'object recognition', 'deep learning', 'neural network'],
-      download_url='https://github.com/thiippal/tankbuster/archive/0.3.tar.gz',
+      download_url='https://github.com/thiippal/tankbuster/archive/0.3.1.tar.gz',
       install_requires=["Keras>=2.0.2",
-			"tensorflow>=1.0.1",
-                        "h5py>=2.6.0"],
+                        "tensorflow>=1.0.1",
+                        "h5py>=2.7.0"],
       classifiers=['Programming Language :: Python :: 2.7'])
